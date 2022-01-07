a5db4bf5531efd5ed7002c0401ea22505d1c30d0
Vimal Mistry <Vimalmistry10@gmail.com>
2022-01-07 18:38:35 +0530

    Update error.php

--- core/config/error.php
+++ core/config/error.php
@@ -3,6 +3,6 @@
 return [
 //        IF true its send notification on database error log
         'send_log' => FALSE,
-        'send_log_mobile' => 9016913088,
-        'send_log_email' => 'vimalmistry@namasteji.in'
+        'send_log_mobile' => 9999999999,
+        'send_log_email' => 'vimalmistry@hello.in'
 ];
